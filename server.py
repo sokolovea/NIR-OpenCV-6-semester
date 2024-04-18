bf1a9448a01c91109d45a4b85d7c6c54cdff13fc
Соколов Егор Александрович <faositb@yandex.ru>
2024-04-18 21:08:23 +0300

    Add reading from file

--- server.py
+++ server.py
@@ -2,9 +2,10 @@ import cv2
 import asyncio
 import websockets
 import base64
+import argparse
 
-async def video_stream(websocket, path):
-    cap = cv2.VideoCapture(0)
+async def video_stream(websocket, path, video_source):
+    cap = cv2.VideoCapture(video_source)
 
     while True:
         ret, frame = cap.read()
@@ -13,11 +14,15 @@ async def video_stream(websocket, path):
 
         # Преобразование кадра в строку base64
         encoded_frame = base64.b64encode(cv2.imencode('.jpg', frame)[1]).decode('utf-8')
-        
+
         # Отправка кадра серверу
         await websocket.send(encoded_frame)
 
-start_server = websockets.serve(video_stream, "localhost", 9999)
+parser = argparse.ArgumentParser(description='Video streaming client')
+parser.add_argument('--source', default=0, help='Video source (0 for webcam, path to file for file)')
+args = parser.parse_args()
 
-asyncio.get_event_loop().run_until_complete(start_server)
+start_client = websockets.serve(lambda websocket, path: video_stream(websocket, path, args.source), "localhost", 9999)
+
+asyncio.get_event_loop().run_until_complete(start_client)
 asyncio.get_event_loop().run_forever()
