3dbf94b2ffb3c60165e2e9f140378dcb1c8dc290
Соколов Егор Александрович <faositb@yandex.ru>
2024-05-02 20:02:59 +0300

    Beta: ready for Pylkin and SYM

--- nonlocal_server.py
+++ nonlocal_server.py
@@ -8,18 +8,18 @@ from ultralytics import YOLO
 import json
 
 
-# define some constants
+# Константы для работы нейросети
 CONFIDENCE_THRESHOLD = 0.8
 GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 
 
 def decode_frame(encoded_frame):
-    # Декодирование строки из формата base64
+    '''
+    Декодирование изображения из формата base64
+    '''
     decoded_bytes = base64.b64decode(encoded_frame)
-    # Преобразование байтов в массив numpy
     nparr = np.frombuffer(decoded_bytes, np.uint8)
-    # Декодирование изображения с помощью cv2.imdecode()
     frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return frame
 
@@ -27,52 +27,58 @@ def decode_frame(encoded_frame):
 # Создание объекта для вычитания фона
 backSub = cv2.createBackgroundSubtractorMOG2()
 
-
-# load the pre-trained YOLOv8n model
+# Загрузка предварительно натренированной модели YOLO
 model = YOLO("yolov8n.pt")
 
 def frame_neuro_processing(frame):
+    '''
+    Обнаружение объектов на кадре с помощью модели YOLOv8n
+    '''
     results = model.track(source=frame, persist=True)
     annotated_frame = results[0].plot()
     return annotated_frame
-    # Обнаружение объектов на кадре с помощью модели YOLOv8n
-    results = model(frame)
-    return frame
 
-min_shadow = 180
-max_shadow = 255
-min_contour_area = 500
+min_brightness = 100 #Нижняя граница яркости распознаваемого участка
+max_brightness = 255 #Верхняя граница яркости распознаваемого участка
+min_contour_area = 500 #Минимальная площадь движущегося объекта для обнаружения
 
 def frame_processing(frame):
+    '''
+    Обработка кадра (обнаружение движения) при помощи OpenCV без применения нейросетей
+    '''
     frame_initial = frame.copy()
-    # Копирование исходного кадра для отрисовки
     frame_out = frame.copy()
+
     # Преобразование изображения в оттенки серого
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     # Вычитание фона
     fg_mask = backSub.apply(frame)
     # Устанавливаем глобальный порог для удаления теней
-    retval, mask_thresh = cv2.threshold(fg_mask, min_shadow, max_shadow, cv2.THRESH_BINARY)
+    retval, mask_thresh = cv2.threshold(fg_mask, min_brightness, max_brightness, cv2.THRESH_BINARY)
     # Поиск контуров
     contours, _ = cv2.findContours(mask_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # Вычисление ядра
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    # Применение эрозии
-    mask_eroded = cv2.morphologyEx(mask_thresh, cv2.MORPH_OPEN, kernel)
+    # # Вычисление ядра
+    # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    # # Применение эрозии
+    # mask_eroded = cv2.morphologyEx(mask_thresh, cv2.MORPH_OPEN, kernel)
     # Фильтрация контуров по минимальной площади
-    large_contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
-    white_image = np.full_like(frame_out, (255, 255, 255), dtype=np.uint8)
-    # Отрисовка прямоугольников вокруг больших контуров
-    for cnt in large_contours:
+    contours = [cnt for cnt in contours if cv2.contourArea(cnt) > min_contour_area]
+    # Фон, на который будут помещаться движущиеся фрагменты изображения
+    cropped_image = np.full_like(frame_out, (255, 255, 255), dtype=np.uint8)
+    # Отрисовка прямоугольников контуров
+    for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
-        white_image[y:y+h, x:x+w] = frame_initial[y:y+h, x:x+w]
+        # Копирование движущейся части изображения на фон
+        cropped_image[y:y+h, x:x+w] = frame_initial[y:y+h, x:x+w]
+        # Отрисовка прямоугольников контуров на выходном изображении
         frame_out = cv2.rectangle(frame_out, (x, y), (x+w, y+h), (200, 0, 0), 2)
-        # Отображаем прямоугольник и подписываем его меткой класса
-    # Отображение результата
-
-    return frame_out, white_image
+    # Возврат выходных кадров
+    return frame_out, cropped_image
 
 async def receive_from_server(websocket, path, key):
+    '''
+    Отправка ответа клиенту
+    '''
     cipher_suite = Fernet(key)
     while True:
         try:
@@ -86,19 +92,18 @@ async def receive_from_server(websocket, path, key):
                         # Дешифровка данных
                         data = cipher_suite.decrypt(data).decode()
                         # Обработка данных
-                        frame = decode_frame(data)  # Декодируем данные
-                        frame, cropped = frame_processing(frame)     # Обрабатываем кадр
+                        frame = decode_frame(data)
+                        frame, cropped_frame = frame_processing(frame)
                         # Преобразование кадра в строку base64
                         data = base64.b64encode(cv2.imencode('.jpg', frame)[1]).decode('utf-8')
 
                         # Создание словаря с данными для JSON
-                        json_data = {
-                            "video_image": data
-                        }
+                        json_data = { "video_image": data }
 
+                        # Если клиент нажал на кнопку для распознавания объектов на кадре,
+                        # то обрабатываем cropped_frame при помощи нейросети
                         if instructions != None and json.loads(instructions).get('recognize'):
-                            json_data["recognize_image"] = base64.b64encode(cv2.imencode('.jpg', frame_neuro_processing(cropped))[1]).decode('utf-8')
-                            # json_data["recognize_image"] = base64.b64encode(cv2.imencode('.jpg', cropped)[1]).decode('utf-8')
+                            json_data["recognize_image"] = base64.b64encode(cv2.imencode('.jpg', frame_neuro_processing(cropped_frame))[1]).decode('utf-8')
 
                         # Преобразование словаря в JSON-строку
                         data = json.dumps(json_data)
@@ -108,32 +113,22 @@ async def receive_from_server(websocket, path, key):
                         #получение данных от клиента (веб-браузера)
                         instructions = await websocket.recv()
                         if instructions:
-                            # print("Получены инструкции:", instructions)
-                            # Попытка парсинга JSON
                             try:
                                 parsed_instructions = json.loads(instructions)
-                                # print("Разобранные инструкции:", parsed_instructions)
-                                global min_shadow
-                                global max_shadow
+                                global min_brightness
+                                global max_brightness
                                 global min_contour_area
-                                min_shadow = float(parsed_instructions.get('min_shadow'))
-                                max_shadow = float(parsed_instructions.get('max_shadow'))
+                                min_brightness = float(parsed_instructions.get('min_brightness'))
+                                max_brightness = float(parsed_instructions.get('max_brightness'))
                                 min_contour_area = float(parsed_instructions.get('min_contour_area'))
-                                recognize = parsed_instructions.get('recognize')
-                                if recognize:
-                                    print("Запрошено распознавание движущихся объектов")
-                                # Далее вы можете обрабатывать распарсированные данные
-                                # Например, выполнить какие-то действия на основе полученных инструкций
                             except json.JSONDecodeError:
                                 print("Ошибка при парсинге JSON.")
                         else:
                             print("Пустой ответ от клиента.")
-
                     except websockets.exceptions.ConnectionClosedError:
                         print("Соединение с сервером на порту 9998 закрыто. Переподключение...")
                         break
                     except Exception as e:
-                        # Логирование и обработка других исключений
                         print(f"Произошла ошибка: {e}")
                         break
         except websockets.exceptions.ConnectionClosedError: #было ConnectionError
@@ -141,9 +136,10 @@ async def receive_from_server(websocket, path, key):
             await asyncio.sleep(1)
         except Exception as e:
             # Логирование и обработка других исключений
-            print(f"Произошла ошибка: {e}")
+            print(f"Произошла ошибка: {e}. Повторная попытка переподключения через 1 секунду...")
             await asyncio.sleep(1)
 
+# Ключ для соединения с сервером трансляции видеопотока
 key = b'2na2SazDd926u9bR5Sn5MBJBsuIsyANgdKERwhyBmag='
 
 async def main(websocket, path):
